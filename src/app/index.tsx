8e0f7953906b067ed1e9197e890e8c535a006274
Kazhtova <kasshhhoktora@gmail.com>
2026-06-25 14:00:00 +0700

    feat: update task tanggal 25 juni -2

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import LatVar from './var';
 
 export default function Index() {
-  // Data pengguna
+  // Data pengguna satu
   // 1
 
   const firstName = "Budi";
